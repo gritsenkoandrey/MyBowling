ae01abc215b5bc2fa34c15f210f86a9f28181d38
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-09-30 23:24:53 +0300

    Добавил класс DoTweenShowText, который отображает текст в игре через DoTween

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -10,11 +10,14 @@ namespace Assets.Scripts
         [SerializeField] private GameObject _botCollisionParticle;
         [Range(0.0f, 5.0f), SerializeField] private float _destroyBotByTime = 0.0f;
         private Ball _ball;
+
         private DoTweenCameraShake _shake;
+        private DoTweenShowText _text;
 
         private void Start()
         {
             _shake = FindObjectOfType<DoTweenCameraShake>();
+            _text = FindObjectOfType<DoTweenShowText>();
         }
 
         private void OnTriggerEnter(Collider other)
@@ -23,13 +26,14 @@ namespace Assets.Scripts
 
             if (_ball)
             {
-                // todo destroy particle
                 Instantiate(_destroyBotParticle, gameObject.transform.position, Quaternion.identity);
                 Instantiate(_botCollisionParticle, gameObject.transform.position, Quaternion.identity);
+
                 if (_shake != null)
-                {
                     _shake.CreateShake();
-                }
+                //if (_text != null)
+                //    _text.ApplyDamage(gameObject.transform.position, 1.0f);
+
                 Invoke(nameof(DestroyBot), _destroyBotByTime);
             }
         }
